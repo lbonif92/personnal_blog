69c7eec1d670b940af223eee34b21ac86ef7a2f4
lbonif92 <loic.boniface@gmail.com>
2023-02-27 19:05:35 +0100

    add new views

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const pepinieresContent = "Etiam vel turpis eu tortor mollis porttitor tincidunt imperdiet odio. Phasellus tempus sapien vel erat blandit pretium. Vestibulum vulputate metus nibh, eget porttitor leo egestas in. Curabitur laoreet sollicitudin lobortis. Nulla nisi metus, tincidunt quis imperdiet ac, porttitor a ex. Nulla elit nunc, porta in massa id, ullamcorper feugiat urna. Etiam vestibulum pellentesque tortor, id congue elit semper a. Integer nulla nisl, sodales a porttitor ut, commodo sed ipsum. Sed a dapibus tortor. In quis sem nec erat pellentesque maximus eu non urna. Pellentesque eget congue tortor. Nam quis enim vel purus bibendum euismod a vestibulum eros.";
+const arbresContent = "Description ..."
 
 // création d'un schéma
 const postSchema = {
@@ -86,6 +88,17 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactText: contactContent});
 })
+
+// GET route 'Pepinieres'
+app.get("/pepinieres", function(req, res){
+  res.render("pepinieres", {introPep: pepinieresContent});
+})
+
+// GET route 'Arbres'
+app.get("/arbres", function(req, res){
+  res.render("arbres", {ArbresText: arbresContent});
+})
+
 
 // GET route' compose' ==> pour écrire du nouveau contenu
 app.get("/compose", function(req, res){
